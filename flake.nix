67bcf605aa12d6c0c925aeeacbc9c68d5d238129
r17x <ri7nz@evilfactory.id>
2025-10-21 19:08:17 +0700

    chore: setup development environment

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Micromeet Project development";
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+        "x86_64-darwin"
+      ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells = {
+            default = pkgs.mkShell {
+              buildInputs = [
+                pkgs.nodejs
+              ];
+            };
+          };
+        };
+    };
+
+  inputs = {
+    # utilities for Flake
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    ## -- nixpkgs
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.follows = "nixpkgs-unstable";
+  };
+}
